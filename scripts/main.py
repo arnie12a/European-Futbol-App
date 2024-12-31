96fc2a697bb8578d6b184fd54cb3e929644e0fc4
Arnav Karnik <38790024+arnie12a@users.noreply.github.com>
2024-12-30 20:56:22 -0600

    added more to the scraping

--- scripts/main.py
+++ scripts/main.py
@@ -1,28 +1,79 @@
 def getDataForYear(year):
+    getFranceData(year)
+    # getEnglandData(year)
+    # getGermanyData(year)
+    # getSpainData(year)
+    # getItalyData(year)
+    
+
+    return 
+
+def getFranceData(year):
     import pandas as pd
-    df = pd.read_html(f'https://fbref.com/en/squads/19538871/{str(year)}-{str(year+1)}/Manchester-United-Stats')[0]
-    # df.columns = pd.MultiIndex.from_tuples(
-    # [('About' if 'Unnamed' in level_0 else level_0, level_1) 
-    #     for level_0, level_1 in df.columns],
-    #         names=df.columns.names
-    # )
-    # df_combined = pd.concat([df['About'], df['Playing Time'], df['Performance']], axis=1)
-    # columns_to_keep = ['Player', 'Nation', 'Pos', 'Age', 'MP', 'Min', 'Gls', 'Ast', 'G+A', 'PK']
-    # df_combined = df_combined[columns_to_keep]
-    return df.head()
+    url = f'https://fbref.com/en/comps/13/{str(year)}-{str(year+1)}/{str(year)}-{str(year+1)}-Ligue-1-Stats'
+    dfs = pd.read_html(url)
+    print(dfs)
+    print(len(dfs))
+    getMetrics(dfs)
+    return 
 
+def getEnglandData(year):
+    url = f'https://fbref.com/en/comps/9/{str(year)}-{str(year+1)}/{str(year)}-{str(year+1)}-Premier-League-Stats'
+    print("England" + str(year))
 
-def getFranceData():
     return
 
-def getEnglandData():
+def getSpainData(year):
+    url = f'https://fbref.com/en/comps/12/{str(year)}-{str(year+1)}/{str(year)}-{str(year+1)}-La-Liga-Stats'
+    print("Spain" + str(year))
+    
     return
 
-def getSpainData():
+def getGermanyData(year):
+    url = f'https://fbref.com/en/comps/20/{str(year)}-{str(year+1)}/{str(year)}-{str(year+1)}-Bundesliga-Stats'
+    print("Germany" + str(year))
+    
     return
 
-def getGermanyData():
+def getItalyData(year):
+    url = f'https://fbref.com/en/comps/11/{year}-{year+1}/{year}-{year+1}-Serie-A-Stats'
+    print("Italy" + str(year))
+    
     return
 
-def getItalyData():
+
+def getGeneralData(dfs):
+    return dfs[0]
+
+
+def squadStandardStats(dfs):
+    return dfs[1]
+
+def squadShooting(dfs):
+    return dfs[4]
+
+def squadPassing(dfs):
+    return dfs[5]
+
+def squadPassType(dfs):
+    return dfs[6]
+
+def squadDefensiveActions(dfs):
+    return dfs[8]
+
+def squadPossession(dfs):
+    return dfs[9]
+
+def squadMiscellaneousStats(dfs):
+    return dfs[11]
+
+def getMetrics(dfs):
+    getGeneralData(dfs)
+    squadStandardStats(dfs)
+    squadShooting(dfs)
+    squadPassing(dfs)
+    squadPassType(dfs)
+    squadDefensiveActions(dfs)
+    squadPossession(dfs)
+    squadMiscellaneousStats(dfs)
     return
