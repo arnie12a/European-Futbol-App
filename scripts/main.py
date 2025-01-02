649d3aba3e8a1fc0758691283a1a06ccf9bc3cdc
Arnav Karnik <38790024+arnie12a@users.noreply.github.com>
2025-01-01 23:10:51 -0600

    added more methods to get the right data scraped

--- scripts/main.py
+++ scripts/main.py
@@ -1,21 +1,18 @@
 def getDataForYear(year):
-    getFranceData(year)
+    
     # getEnglandData(year)
     # getGermanyData(year)
     # getSpainData(year)
     # getItalyData(year)
     
 
-    return 
+    return getFranceData(year)
 
 def getFranceData(year):
     import pandas as pd
     url = f'https://fbref.com/en/comps/13/{str(year)}-{str(year+1)}/{str(year)}-{str(year+1)}-Ligue-1-Stats'
     dfs = pd.read_html(url)
-    print(dfs)
-    print(len(dfs))
-    getMetrics(dfs)
-    return 
+    return squadShooting(dfs)
 
 def getEnglandData(year):
     url = f'https://fbref.com/en/comps/9/{str(year)}-{str(year+1)}/{str(year)}-{str(year+1)}-Premier-League-Stats'
@@ -43,14 +40,35 @@ def getItalyData(year):
 
 
 def getGeneralData(dfs):
-    return dfs[0]
-
+    df = dfs[0]
+    columns = ['Rk', 'Squad', 'MP', 'W', 'L', 'GF', 'GA', 'GD', 'Pts', 'Pts/MP', 'Top Team Scorer']
+    filtered_df = df[columns]
+    return filtered_df
 
 def squadStandardStats(dfs):
-    return dfs[1]
+    import pandas as pd
+    df = dfs[2]
+    df.columns = pd.MultiIndex.from_tuples(
+    [('About' if 'Unnamed' in level_0 else level_0, level_1) 
+        for level_0, level_1 in df.columns],
+            names=df.columns.names
+    )
+
+    df_combined = pd.concat([df['About'], df['Performance']], axis=1)
+    columns_to_keep = ['Squad', '# Pl', 'Age', 'Gls', 'Ast', 'G+A', 'PK', 'PKatt', 'CrdY', 'CrdR']
+    return df_combined[columns_to_keep]
 
 def squadShooting(dfs):
-    return dfs[4]
+    import pandas as pd
+    df = dfs[8]
+    df.columns = pd.MultiIndex.from_tuples(
+    [('About' if 'Unnamed' in level_0 else level_0, level_1) 
+        for level_0, level_1 in df.columns],
+            names=df.columns.names
+    )
+    df_combined = pd.concat([df['About'], df['Standard']], axis=1)
+    columns_to_keep = ['Squad', 'Gls', 'Sh', 'SoT', 'SoT%', 'Sh/90', 'SoT/90', 'G/Sh', 'G/SoT', 'Dist', 'FK', 'PK', 'PKatt']
+    return df_combined[columns_to_keep]
 
 def squadPassing(dfs):
     return dfs[5]
